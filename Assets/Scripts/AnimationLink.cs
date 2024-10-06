832088a9f8a4780b99a128b0fdb9e319835aa383
Sristy <152259072+SristyGP@users.noreply.github.com>
2024-10-06 11:43:37 +0200

    intento de solucionar el maintance

--- Assets/Scripts/AnimationLink.cs
+++ Assets/Scripts/AnimationLink.cs
@@ -7,12 +7,15 @@ public class AnimationLink : MonoBehaviour
     private string m_AnimationSpeedParameterName;
     private Animator m_AnimatorComponent;
     private UnityEngine.AI.NavMeshAgent m_NavAgentComponent;
+    private float originalSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
        m_AnimatorComponent = transform.GetChild(0).GetComponent<Animator>();
        m_NavAgentComponent = GetComponent<NavMeshAgent>();
+        originalSpeed = m_NavAgentComponent.speed;
+
     }
     
 
@@ -21,5 +24,22 @@ public class AnimationLink : MonoBehaviour
     {
         m_AnimatorComponent.SetFloat(m_AnimationSpeedParameterName, m_NavAgentComponent.speed);
     }
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Maintenance") && gameObject.name == "Thief")
+        {
+            m_NavAgentComponent.speed = originalSpeed * 0.5f;  // Reducir la velocidad en un 50%
+        }
+    }
+
+    // Detectar cuando el agente sale de la zona "Maintenance"
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Maintenance") && gameObject.name == "Thief")
+        {
+            m_NavAgentComponent.speed = originalSpeed;  // Restaurar la velocidad original
+        }
+    }
+
 }
