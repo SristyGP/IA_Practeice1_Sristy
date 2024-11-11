0cc217f7317d91cd49980cd01ed2c0737be6abc9
SristyGP <152259072+SristyGP@users.noreply.github.com>
2024-11-11 17:07:33 +0100

    AIDirector

---
+++ Assets/Scripts/UI/AiDirector.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class AiDirector : MonoBehaviour
+{
+    public static AiDirector instance;
+    public Transform[]  Isalarmining;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = new AiDirector(); // si no se detecta el game manager, crea uno nuevo. Si no se crea uno nuevo, este se destruye.
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
